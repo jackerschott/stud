1c874790e356083d090a66560da0a126edea4294
jackerschott <jona@einstein.localdomain>
2019-10-24 11:48:07 +0200

    src, config, setup: Change directory structure.
    The config files will be merged to one file, which will be generated by
    a new installation script.

--- src/browserShow.py
+++
@@ -1,9 +0,0 @@
-import os
-import subprocess as sp
-
-def openUrl(url):
-  NULL = open(os.devnull, 'w')
-  sp.Popen(['nohup', 'sensible-browser', url], stdout=NULL, stderr=NULL)
-
-def openFile(path):
-  openUrl('file://' + path)
--- src/servers/theo4.py
+++
@@ -1,7 +0,0 @@
-
-serverUrl = 'https://www.thphys.uni-heidelberg.de'
-scriptUrl = serverUrl + '/~weigand/Skript-QM2011/skript.pdf'
-
-def getScript(session):
-  resp = session.get(scriptUrl)
-  return resp.content
